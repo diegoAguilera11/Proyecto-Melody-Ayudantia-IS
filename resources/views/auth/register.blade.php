b61736ad4f40b06c83bfcc51951a5cd9863eb8d0
diegoAguilera11 <diego.aguilera@alumnos.ucn.cl>
2023-04-15 20:54:37 -0400

    ayudantía 15-04

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.app')
+@section('title')
+    Registrar Cliente
+@endsection
+
+@section('content')
+    <div class="md:flex md:justify-center">
+        <div class="md:w-1/2 bg-gray-800 p-6 rounded-lg shadow-lg">
+
+            <h2 class="text-center text-white uppercase font-bold text-3xl p-4">Registrate en Melody</h2>
+            <form action="{{ route('register') }}" method="POST" novalidate>
+                @csrf
+                <div class="mb-5">
+                    <label for="name" class="mb-2 block uppercase text-white font-bold">
+                        Nombre
+                    </label>
+                    <input id="name" name="name" placeholder="Ingresa tu nombre"
+                        class="border p-2 rounded-lg w-full
+                @error('name') border-red-600
+                @enderror">
+                    @error('name')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-lg text-center p-2">{{ $message }}</p>
+                    @enderror
+                </div>
+
+                <div class="mb-5">
+                    <label for="email" class="mb-2 block uppercase text-white font-bold">
+                        Correo Electrónico
+                    </label>
+                    <input id="email" name="email" placeholder="correo@correo.com"
+                        class="border p-2 rounded-lg w-full
+                @error('email') border-red-600
+                @enderror">
+                    @error('email')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-lg text-center p-2">{{ $message }}</p>
+                    @enderror
+                </div>
+
+                <div class="mb-5">
+                    <label for="password" class="mb-2 block uppercase text-white font-bold">
+                        Contraseña
+                    </label>
+                    <input id="password" name="password" type="password"
+                        class="border p-2 rounded-lg w-full
+                @error('password') border-red-600
+                @enderror">
+                    @error('password')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-lg text-center p-2">{{ $message }}</p>
+                    @enderror
+                </div>
+
+                <input type="submit" value="Registrar"
+                    class="bg-yellow-400 hover:bg-yellow-500 transition-colors cursor-pointer uppercase font-bold text-black rounded w-full p-3">
+            </form>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('title')
+    Inicio
+@endsection
+
+@section('content')
+    <h1 class="text-3xl text-center uppercase font-bold">Bienvenido a Melody</h1>
+@endsection
